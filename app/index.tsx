81bf930f4df44e0d5faf255e45135d30977e8671
Artem Ivannikov <a.ivannikov@aladdin-rd.ru>
2021-09-11 00:08:59 +0300

    Library rechart is included. Styles changed. When choosing a device, graph is drawing of parameters, which is numeric and which are not specified in array PARAMS

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,49 @@ import React from "react";
 import ReactDom from "react-dom";
 import { Device, getDevices, getPackets, Packet } from "./api";
 
+import {
+  LineChart,
+  Line,
+  XAxis,
+  Tooltip,
+  YAxis,
+  Brush,
+  Legend,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+
 import "./index.css";
+
+const COLORS = [
+  "#1890FF",
+  "#2FC25B",
+  "#FACC14",
+  "#738AE6",
+  "#8543E0",
+  "#DD81E6",
+  "#FA7D92",
+  "#F04864",
+  "#13C2C2",
+];
+
+const PARAMS = [
+  "_bot",
+  "_gid",
+  "_id",
+  "_mid",
+  "_msgid",
+  "_oid",
+  "_psd",
+  "_ts",
+  "_web",
+  "id",
+  "online",
+  "payload",
+  "service",
+  "time",
+  "topic",
+];
 
 document.title = "Hello";
 
@@ -78,6 +120,64 @@ class App extends React.Component<{}, AppState> {
 
     const device_name = (this.state.device?.name);
 
+    console.log(packet);
+
+    const keys = Object.keys(packet).map(key => {
+      if (!isNaN(packet[key])) {
+        if (isFinite(packet[key])) {
+          if (typeof(packet[key]=="number")) {
+            return key;
+          }
+        }
+      }
+    })
+    console.log(keys);
+
+    const params = keys.filter(function( element ) {
+      return (element !== undefined) && (!PARAMS.includes(element));
+    });
+
+    console.log(params);
+
+    const values = params.map((k) => (
+      <ValueBox key={k} title={k} value={packet[k]} />
+    ));
+
+    console.log(values);
+
+    const lines = params.map((k, i) => (
+      <Line
+        key={k}
+        stroke={COLORS[i]}
+        type="linear"
+        dataKey={k}
+        dot={false}
+        isAnimationActive={false}
+      />
+    ));
+
+    const chart = (
+      <ResponsiveContainer>
+        <LineChart data={this.state.packets}>
+          {lines}
+
+          <Legend />
+          <CartesianGrid />
+
+          <XAxis
+            dataKey="time"
+            tickFormatter={(x) => {
+              if (x === "auto") return ".";
+              return new Date(x).toLocaleString();
+            }}
+          />
+          <Brush />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+
     return (
       <div className="app">
         <div className="head"></div>
@@ -94,6 +194,13 @@ class App extends React.Component<{}, AppState> {
             )}
             {device_name}
           </div>
+          <div className="value-boxes">
+            {values}
+            {values.length === 0 && (
+              <span>нет данных</span>
+            )}
+          </div>
+          {chart}
         </div>
       </div>
     );
